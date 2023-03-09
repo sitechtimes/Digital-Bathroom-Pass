9f55d84ad3250520cac8680a290fc4e631736be5
EdwinZ11 <90789780+EdwinZ11@users.noreply.github.com>
2023-03-09 12:31:20 -0500

    url schemes

--- bathroomPass/src/views/TestPage.vue
+++ bathroomPass/src/views/TestPage.vue
@@ -4,6 +4,7 @@
             <div class="container">
                 <a href="http://localhost:8100/" >testing links 1</a>
                 <a href="http://localhost:8100/SignIn" >testing links 2</a>
+                <ion-button @click="testClick">click test</ion-button>
             </div>
         </ion-content>
     </ion-page>
@@ -11,24 +12,33 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import { IonPage, IonContent } from '@ionic/vue'
+import { IonPage, IonContent, IonButton } from '@ionic/vue'
 
 export default defineComponent({
     name: "TestPage",
     components: {
         IonPage,
-        IonContent
-    },
-    methods: {
-        //
+        IonContent,
+        IonButton
     },
     data() {
         return {
-            testUrl: "http://localhost:8100/home/123"
+            testUrl: "http://localhost:8100/home/123",
+            splitUrl: ""
         }
-    } 
+    },
+    methods: {
+        testClick() {
+            var testForHome = "home"
 
-
+            if(this.testUrl.includes(testForHome)) {
+                const Bongbing = this.testUrl.split("/home/").pop()
+                this.splitUrl = Bongbing!
+            } else {
+                console.log("herm")
+            }
+        }
+    }
 })
 
 </script>
