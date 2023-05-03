af3b274f1c265e3edcd3e3e412f76bfff16b5ccd
arshmeets <arshmeetsodhi@gmail.com>
2023-05-03 14:35:56 -0400

    Updated Requirements.txt file and deleted deta files

--- backend/bathroom_pass/main.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-import backend
-
-app = FastAPI()
-
-@app.get("/getStatus/{room_id}")
-async def read_item(room_id):
-    message = backend.readDoc(room_id)
-    return {"message" : message}
