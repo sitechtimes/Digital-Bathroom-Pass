811c6e17f454fcacac81bbd81f3edd678e665651
Johnson Wang <johnsonwang529@gmail.com>
2023-07-12 12:55:40 -0400

    sdccsd

---
+++ bathroomPass/src/views/ClassRoom.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script lang="ts" setup>
+import { onMounted } from "vue";
+import { useRoute } from "vue-router";
+
+onMounted(() => {
+    //
+    const router = useRoute()
+    console.log("ClassRoom.vue mounted", router.params.id )
+})
+</script>
+
+<style lang="scss" scoped>
+
+</style>
