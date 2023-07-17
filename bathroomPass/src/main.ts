d7eade0e9b9739e5333e804b71c31be255486ab6
EdwinZ11 <90789780+EdwinZ11@users.noreply.github.com>
2023-07-17 11:13:38 -0400

    Revert "."
    This reverts commit 3cdb12fa89ae4ac498df53b9c4111818128a828a.

--- bathroomPass/src/main.ts
+++ bathroomPass/src/main.ts
@@ -19,7 +19,7 @@ import '@ionic/vue/css/text-alignment.css';
 import '@ionic/vue/css/text-transformation.css';
 import '@ionic/vue/css/flex-utils.css';
 import '@ionic/vue/css/display.css';
-import './registerServiceWorker.ts'
+
 /* Theme variables */
 import './theme/variables.css';
 
