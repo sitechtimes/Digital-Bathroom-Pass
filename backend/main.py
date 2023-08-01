dcdc81d1f1e719d04cce35abc9777251000d341e
EdwinZ11 <90789780+EdwinZ11@users.noreply.github.com>
2023-08-01 12:36:33 -0400

    ss

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,6 @@
 import os
-from datetime import datetime, timedelta
-
 import gspread
+from datetime import datetime, timedelta
 from dotenv import load_dotenv
 from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
